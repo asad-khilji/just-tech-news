ea4ee0fbc4cdcd51fb5e947f172025d398e7928c
Asad Khilji <khiljiasad2@gmail.com>
2022-06-11 16:17:19 -0400

    updated

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -84,6 +84,16 @@ router.get('/', (req, res) => {
       });
   });
 
+  // PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => res.json(err));
+});
+
   router.delete('/:id', (req, res) => {
     Post.destroy({
       where: {
